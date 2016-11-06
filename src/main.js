cda28da9e05191b5b8872fb16fb005f693e3c0a6
Vivek Tikar <vivek@vivek-laptop>
2016-11-06 13:49:17 +0530

    changed one file for exp

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const app = electron.app;
 const browserWindow = electron.BrowserWindow;
 const {dialog} = require('electron')
 const reload = require('electron-reload');
-
+ 
 app.on('ready', function(){
 	var editorWindow = new browserWindow({height:700});
 	editorWindow.loadURL("file://"+`${__dirname}`+"/editor.html");	
